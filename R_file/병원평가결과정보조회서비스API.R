def3dff15d47770323ad4eeab1760c2de1abe227
Helloworldist <33469961+Helloworldist@users.noreply.github.com>
2018-04-09 15:36:25 +0900

    호출 최적화

--- R_file/병원평가결과정보조회서비스API.R
+++ R_file/병원평가결과정보조회서비스API.R
@@ -10,23 +10,23 @@ library(plyr)
 url_eval_1<-"http://apis.data.go.kr/B551182/hospAsmRstInfoService/"
 category_eval<- "getHospWhlAsmRstList"
 url_eval_2<-"?serviceKey="
-key<-"8H4WapzXJEl6ly5SVzhKi6418TEKuE5Mdk3Wj8PrYfGSv8nW1ZsJO2Plmh5oA1jZzg42rqaQy6%2FNnDRUZrdbdQ%3D%3D"
+key_eval<-"S9WEvSCrAevu5g77dqb0UyGoWrFdu8D7yyldkqgRFhJOL1Z%2Fow7c5RFe8Crs5ycZIdREt%2BhclvTNlTDd%2BBAPRg%3D%3D"
 url_eval_3<-"&pageNo=1"
 url_eval_4<-"&ykiho="
 
 table_eval<- data.frame()
 ykNum<-nrow(ykihoList)
 
-requestURL_eval <- paste(url_eval_1, category_eval, url_eval_2, key, url_eval_3, url_eval_4, sep="")
+requestURL_eval <- paste(url_eval_1, category_eval, url_eval_2, key_eval, url_eval_3, url_eval_4, sep="")
 
-#####################################
+############### 평가결과 긁어오기 ################
 
-for(a in 1:ykNum)
+for(a in 1:totalDN)
 {
   page = getForm(paste(requestURL_eval, ykihoList[a,1], sep= ""), query="")
   doc1=xmlParse(page)
   doc1=xmlToList(doc1)
-
+  
   ex <- data.frame(doc1[2]$body$items[1])
   if(length(table)==0)
   {
@@ -36,6 +36,43 @@ for(a in 1:ykNum)
     table_eval<-rbind.fill(table_eval, ex)
 }
 
-######################################3
+
+
+
+######### 에러 발생 시 누락없이 긁어오기 ############
+
+run=1
+for(a in run:100)
+{
+  page = getForm(paste(requestURL_eval, ykihoList[a,1], sep= ""), query="")
+  doc1=xmlParse(page)
+  doc1=xmlToList(doc1)
+  
+  ex <- data.frame(doc1[2]$body$items[1])
+  if(is.null(doc1[2]$body$items[1]))
+  {
+    print(paste("NULL 발생 : ", a, sep=""))
+    while(1){
+      page = getForm(paste(requestURL_eval, ykihoList[a,1], sep= ""), query="")
+      doc1=xmlParse(page)
+      doc1=xmlToList(doc1)
+      
+      ex <- data.frame(doc1[2]$body$items[1])
+      if(is.null(doc1[2]$body$items[1])==FALSE)
+        break
+    }
+  }
+  if(length(table)==0)
+  {
+    table_eval<-ex
+  }
+  else
+    table_eval<-rbind.fill(table_eval, ex)
+  print(a)
+  run=a+1
+}
+
+
+############ 테이블 통합 ###########3
 
 table_united1<-cbind(table_info, table_eval)
