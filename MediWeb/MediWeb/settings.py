2443c30539d5d4c3ff3f946a64ad20b559256b5d
김민중 <kl4314@likelion.org>
2018-04-19 13:25:26 +0900

    검색어 자동 완성 기능 및 dbms 변경

--- MediWeb/MediWeb/settings.py
+++ MediWeb/MediWeb/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'MediWeb.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'capstone',
+        'USER': 'kl4314',
+        'PASSWORD': 'kl43114',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
