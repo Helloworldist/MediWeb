9f9591e6c85fafc974f0e21db7f875671609f937
syoung9606 <37612371+syoung9606@users.noreply.github.com>
2018-05-15 23:43:21 -0700

    백엔드에서 분석결과 python 연동

--- MediWeb/searchapp/models.py
+++ MediWeb/searchapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class List(models.Model):
+	name = models.CharField(max_length = 20)
+	X = models.CharField(max_length = 20)
+	Y = models.CharField(max_length = 20)
