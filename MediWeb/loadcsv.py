d1bc36d24555fe95a45fe35b4d3cec2436257569
huinee <37612362+huinee@users.noreply.github.com>
2018-05-22 14:57:18 +0900

    정보호출 테스트 소스 수정

--- MediWeb/loadcsv.py
+++ MediWeb/loadcsv.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from moreinfo import getDetailinfo1, getDetailinfo2, getDetailinfo3
 
 resultOfHos = []
 
@@ -13,6 +14,15 @@ print("read done.")
 file.close()
 
 for i in range(1,10):
+	print('========='+str(i)+'=========')
 	print(resultOfHos[i-1][18])	# 병원명
 	print(resultOfHos[i-1][16]) # 지도 X좌표
 	print(resultOfHos[i-1][17]) # 지도 Y좌표
+	print(resultOfHos[i-1][0]) # ykiho
+	getDetailinfo1(resultOfHos[i-1][0])
+	print('----')
+	getDetailinfo2(resultOfHos[i-1][0])
+	print('----')
+	getDetailinfo3(resultOfHos[i-1][0])
+	print('----')
+
